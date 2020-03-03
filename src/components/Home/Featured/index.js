a9b7954e02e58845b4dd161a5ae513f68513a539
Alex Wong <alexw14@gmail.com>
2020-03-02 22:17:41 -0800

    add ui/textBlock

--- src/components/Home/Featured/index.js
+++ src/components/Home/Featured/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import TextBlock from '../../ui/textBlock.js';
 import kbMainBackgroundImg from '../../../assets/images/kbmain.jpg';
 
 const Featured = () => {
 
   return (
     <div class="featured-main" style={{backgroundImage: `url(${kbMainBackgroundImg})`}}>
-      
+      <TextBlock text={'KOBE'}/>
     </div>
   );
 };
---
+++ src/components/ui/textBlock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TextBlock = (props) => {
+
+  return (
+    <div className="text-block">
+      {props.text}
+    </div>
+  );
+};
+
+export default TextBlock;
