b9a337fbefbc97b38a2073f2ffc236a508d5ec50
Alex Wong <alexw14@gmail.com>
2020-03-25 22:10:06 -0700

    update project

---
+++ app.js
@@ -0,0 +1,6 @@
+const root = document.querySelector('#root');
+const heroSection = document.createElement('div');
+heroSection.innerHTML = `
+  <div class="hero kb-main"></div>
+`;
+root.appendChild(heroSection);
